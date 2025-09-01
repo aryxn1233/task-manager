fd74c08261ad54711f4f29837e407c548e88638e
Aryan Thakur <156740594+aryxn1233@users.noreply.github.com>
2025-09-02 01:19:07 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, saveFcmToken } = require('../controllers/auth');
-const authMiddleware = require('../middleware/auth.js'); 
+const { protect } = require('../middleware/auth'); // <-- destructure protect
 
 // Auth routes
 router.post('/register', register);
 router.post('/login', login);
 
 // Save FCM token (protected route)
-router.post('/save-fcm-token', authMiddleware, saveFcmToken);
+router.post('/save-fcm-token', protect, saveFcmToken);
 
 module.exports = router;
