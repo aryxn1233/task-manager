415ad41c86fe65a629ba6cd879db23549fdd0ef6
Aryan Thakur <156740594+aryxn1233@users.noreply.github.com>
2025-09-02 00:58:21 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,10 +2,17 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-exports.register = async (req, res, next) => {
+//     Register user
+// POST /api/auth/register
+exports.register = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ success: false, error: 'User already exists' });
+    }
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -33,34 +40,47 @@ exports.register = async (req, res, next) => {
   }
 };
 
-exports.login = async (req, res, next) => {
+//    Login user
+//   POST /api/auth/login
+exports.login = async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
     return res.status(400).json({ success: false, error: 'Please provide an email and password' });
   }
 
-  const user = await User.findOne({ email }).select('+password');
-  if (!user) {
-    return res.status(401).json({ success: false, error: 'Invalid credentials' });
+  try {
+    const user = await User.findOne({ email }).select('+password');
+    if (!user) {
+      return res.status(401).json({ success: false, error: 'Invalid credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ success: false, error: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+    });
+
+    res.status(200).json({
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        fcmTokens: user.fcmTokens, 
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, error: 'Server error' });
   }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) {
-    return res.status(401).json({ success: false, error: 'Invalid credentials' });
-  }
-
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  });
-
-  res.status(200).json({
-    success: true,
-    token,
-    user: {
-      id: user._id,
-      name: user.name,
-      email: user.email,
-    },
-  });
 };
+
+
+exports.saveFcmToken = async (req, res) => {
+  try {
+    const userId = req.user.id; 
+    const { token
