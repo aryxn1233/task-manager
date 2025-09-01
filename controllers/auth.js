8ff846ac8ab4c808479b30a772a327c92a98b88b
Aryan Thakur <156740594+aryxn1233@users.noreply.github.com>
2025-09-02 00:28:47 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,18 +6,15 @@ exports.register = async (req, res, next) => {
   const { name, email, password } = req.body;
 
   try {
-    // Hash password before saving
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create user
     const user = await User.create({
       name,
       email,
       password: hashedPassword,
     });
 
-    // Create token
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: '30d',
     });
@@ -25,6 +22,11 @@ exports.register = async (req, res, next) => {
     res.status(201).json({
       success: true,
       token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
     });
   } catch (error) {
     res.status(400).json({ success: false, error: error.message });
@@ -34,26 +36,20 @@ exports.register = async (req, res, next) => {
 exports.login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  // Validate emil & password
   if (!email || !password) {
     return res.status(400).json({ success: false, error: 'Please provide an email and password' });
   }
 
-  // Check for user
   const user = await User.findOne({ email }).select('+password');
-
   if (!user) {
     return res.status(401).json({ success: false, error: 'Invalid credentials' });
   }
 
-  // Check if password matches
   const isMatch = await bcrypt.compare(password, user.password);
-
   if (!isMatch) {
     return res.status(401).json({ success: false, error: 'Invalid credentials' });
   }
 
-  // Create token
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
@@ -61,5 +57,10 @@ exports.login = async (req, res, next) => {
   res.status(200).json({
     success: true,
     token,
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
   });
 };
